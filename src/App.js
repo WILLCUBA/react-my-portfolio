38e771a5cc8e4c8658c40b24f631613174064376
Will <wilito1210@gmail.com>
2022-05-13 21:12:40 -0400

    about me section done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Nav from './components/Nav';
 import AboutMe from './components/About-Me';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
 function App() {
 
   const [currentPage, setCurrentPage] = useState("About-Me")
@@ -20,16 +23,16 @@ function App() {
         <Portfolio></Portfolio>
       )
     }
-    // if (currentPage === "Contact") {
-    //   return (
-    //     <Contact></Contact>
-    //   )
-    // }
-    // if (currentPage === "Resume") {
-    //   return (
-    //     <Resume></Resume>
-    //   )
-    // }
+    if (currentPage === "Contact") {
+      return (
+        <Contact></Contact>
+      )
+    }
+    if (currentPage === "Resume") {
+      return (
+        <Resume></Resume>
+      )
+    }
   }
   
   const handlePageChange = (page) => setCurrentPage(page)
@@ -38,14 +41,14 @@ function App() {
     <div className="App">
       <header className="m-1 w-100">
         <div className="flex-column text-center m-1 w-100">
-            <img src={require('./assets/myavatar.png')} alt="avatar"></img>
+            <img src={require('./assets/imgs/myavatar.png')} alt="avatar"></img>
             <p>Hello, I'm</p>
             <h1>William Pinero Gonzalez</h1>
             <p>Full Stack Web Developer</p>
         </div>
         <Nav currentPage={currentPage} handlePageChange={handlePageChange}/>
         {renderPage()}
-        </header>
+      </header>
         <Footer></Footer>
     </div>
   );
--- src/components/About-Me/index.js
+++ src/components/About-Me/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 function AboutMe() {
     return (
-        <div className="container">
-            <p>
-                lorem ipsum dolor sit amet, consectetur adip
-                lorem ipsum dolor sit amet, consectetur
-                lorem ipsum dolor sit amet, consectet
-            </p>
-        </div>
+<section class="section about-section gray-bg" id="about">
+            <div class="container">
+                <div class="row align-items-center flex-row-reverse">
+                    <div class="col-lg-6">
+                        <div class="about-text go-to">
+                            <h3 class="dark-color">About Me</h3>
+                            <h6 class="theme-color lead">Full Stack &amp; Web Developer</h6>
+                            <p>Full Stack Web Developer passionate about programming and logic. Recently earned a certificate in full-stack development from the University of Miami, with newly developed skills in JavaScript, CSS, React.js, responsive web design, and databases. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I worked on a team of four to develop a single-page MERN app that helps local business owners track their sales, inventory, and attendance at specialty events. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
