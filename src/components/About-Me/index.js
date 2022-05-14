697ecc804cc4a9e6d7436447d464a3bd39d29cf2
Will <wilito1210@gmail.com>
2022-05-14 00:05:27 -0400

    portfolio section done without the repo linksgit add .

--- src/components/About-Me/index.js
+++ src/components/About-Me/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import '../../assets/css/AboutMe.css';
 
 function AboutMe() {
     return (
-<section class="section about-section gray-bg" id="about">
-            <div class="container">
-                <div class="row align-items-center flex-row-reverse">
-                    <div class="col-lg-6">
-                        <div class="about-text go-to">
-                            <h3 class="dark-color">About Me</h3>
-                            <h6 class="theme-color lead">Full Stack &amp; Web Developer</h6>
+<section className="section about-section gray-bg" id="about">
+            <div className="container">
+                <div className="row align-items-center flex-row-reverse">
+                    <div className="col-lg-6">
+                        <div className="about-text go-to">
+                            <h3 className="dark-color">About Me</h3>
+                            <h6 className="theme-color lead">Full Stack &amp; Web Developer</h6>
                             <p>Full Stack Web Developer passionate about programming and logic. Recently earned a certificate in full-stack development from the University of Miami, with newly developed skills in JavaScript, CSS, React.js, responsive web design, and databases. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I worked on a team of four to develop a single-page MERN app that helps local business owners track their sales, inventory, and attendance at specialty events. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.</p>
                         </div>
                     </div>
