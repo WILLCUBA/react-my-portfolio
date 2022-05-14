6733ba402b033c2d7a4e063609e06c8eba75eea2
Will <wilito1210@gmail.com>
2022-05-13 20:52:10 -0400

    nav bar done

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
+import Nav from './components/Nav';
+import AboutMe from './components/About-Me';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+function App() {
+
+  const [currentPage, setCurrentPage] = useState("About-Me")
+
+  const renderPage = () => {
+    if(currentPage === "About-Me") {
+      return (
+        <AboutMe></AboutMe>
+      )
+    }
+    if (currentPage === "Portfolio") {
+      return (
+        <Portfolio></Portfolio>
+      )
+    }
+    // if (currentPage === "Contact") {
+    //   return (
+    //     <Contact></Contact>
+    //   )
+    // }
+    // if (currentPage === "Resume") {
+    //   return (
+    //     <Resume></Resume>
+    //   )
+    // }
+  }
+  
+  const handlePageChange = (page) => setCurrentPage(page)
+
+  return (
+    <div className="App">
+      <header className="m-1 w-100">
+        <div className="flex-column text-center m-1 w-100">
+            <img src={require('./assets/myavatar.png')} alt="avatar"></img>
+            <p>Hello, I'm</p>
+            <h1>William Pinero Gonzalez</h1>
+            <p>Full Stack Web Developer</p>
+        </div>
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange}/>
+        {renderPage()}
+        </header>
+        <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About-Me/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AboutMe() {
+    return (
+        <div className="container">
+            <p>
+                lorem ipsum dolor sit amet, consectetur adip
+                lorem ipsum dolor sit amet, consectetur
+                lorem ipsum dolor sit amet, consectet
+            </p>
+        </div>
+    )
+}
+
+
+export default AboutMe
